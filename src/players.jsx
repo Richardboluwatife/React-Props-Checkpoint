3962bacb6814e4ce9574f9eace10b17b0150a31b
Richardboluwatife <richardfadareboluwatife@gmail.com>
2023-11-29 16:12:56 +0100

    first commit

---
+++ src/players.jsx
@@ -0,0 +1,81 @@
+// players.js
+
+
+
+const players = [
+
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 36,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT2v0dWrBFNjcarJM0ljeZeBcCON3OUWfR7bo93AJIWJiG4qCqdP3dRq7O3-SF-hRK0M4&usqp=CAU",
+  },
+
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentina",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHOlr3wFB8PRErFQlecpClAXWUkTVJecaiaA&usqp=CAU",
+  },
+
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 23,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRee2Zc2J13kkNawVjnoHLWWK0S1fwVAxxZbg&usqp=CAU",
+  },
+
+  {
+    name: "Neymar da Silva Santos Júnior",
+    team: "PSG",
+    nationality: "Brazil",
+    jerseyNumber: 10,
+    age: 31,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwabFv0S-1K2JgMgYqSGOgcw8iH7_tpND2bI6Qf-8U_SskD646KqbMtRrHqqEq5rQZh_0&usqp=CAU",
+  },
+
+  {
+    name: "Kylian Mbappe",
+    team: "PSG",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 24,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxCO4S3zEsLhVsCv5aFOQpczQAPmxTYr0hE6bymCffPkunsniTgsSb_7GddktUXGkX6to&usqp=CAU",
+  },
+
+  {
+    name: "Vinicius Junior",
+    team: "Real Madrid",
+    nationality: "Brazil",
+    jerseyNumber: 7,
+    age: 23,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3_PKluD9t6f5atQujPf2oopjqQAWcGPWyKw&usqp=CAU",
+  },
+
+  {
+    name: "Mohamed Salah",
+    team: "Liverpool",
+    nationality: "Egypt",
+    jerseyNumber: 11,
+    age: 31,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGM3MtdJ5Aw2RHGNB6ohtJxPUunBxGCXen5g&usqp=CAU",
+  },
+
+  {
+    name: "Bukayo Saka",
+    team: "arsenal",
+    nationality: "Nigeria",
+    jerseyNumber: 7,
+    age: 22,
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnF52SxN8TP88JG0Y3vQ_mUXVT5wfCZEXEeg&usqp=CAU",
+  },
+  // Add more players here
+];
+
+export default players;
